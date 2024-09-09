7096373628633e52566735e39059b8cc8c7247b6
joobumjin <bumjin.joo@gmail.com>
2024-09-08 21:54:26 -0400

    fix: more realistic resoruces for gpu test

--- run_debug.sh
+++ run_debug.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu-debug --gres=gpu:1
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 00:01:00
-#SBATCH --mem=1g
+#SBATCH -t 00:10:00
+#SBATCH --mem=10g
 
 # Load a CUDA module
 module load cuda
@@ -15,4 +15,4 @@ conda activate qbam
 
 # Run program
 cd /users/bjoo2/code/qbam/qbam_gnn
-python3 grid_search.py --data /users/bjoo2/data/bjoo2/qbam/data --pred TER --chkpt_path /users/bjoo2/data/bjoo2/qbam/checkpoints --img_path /users/bjoo2/data/bjoo2/qbam/graphs
+python3 grid_search.py --data /users/bjoo2/data/bjoo2/qbam/data --pred TER --chkpt_path /users/bjoo2/data/bjoo2/qbam/checkpoints --img_path /users/bjoo2/data/bjoo2/qbam/graphs --results_path /users/bjoo2/data/bjoo2/qbam/text_out
