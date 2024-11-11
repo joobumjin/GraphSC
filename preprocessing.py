506ea7e5947661412a31ad5327b9471e4de6106d
joobumjin <bumjin.joo@gmail.com>
2024-11-11 15:44:26 -0500

    change order of ops

--- preprocessing.py
+++ preprocessing.py
@@ -95,11 +95,11 @@ def get_image_loaders(base_dir, data_dirs, target, batch_size):
         """
         In our cases, we want to collate a list of Data instances
         """
-        images = np.transpose(np.array([sample.x for sample in data]), axes=(0,3,1,2))
-        labels = np.array([sample.y for sample in data])
+        images = torch.Tensor(np.transpose(np.array([sample.x for sample in data]), axes=(0,3,1,2)))
+        labels = torch.Tensor(np.array([sample.y for sample in data]))
         print(f"Collated Labels: {labels}")
 
-        return Data(crop(torch.Tensor(images)), torch.Tensor(labels))
+        return Data(crop(images), labels)
 
     train_csv = data_dirs["train"]
     test_csv = data_dirs["test"]
