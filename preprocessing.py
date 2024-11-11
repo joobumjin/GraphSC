17b12450026c7e7203fd325ada39d906e51e18b8
joobumjin <bumjin.joo@gmail.com>
2024-11-11 15:34:24 -0500

    fix: matching torchgeometric api

--- preprocessing.py
+++ preprocessing.py
@@ -101,8 +101,6 @@ def get_image_loaders(base_dir, data_dirs, target, batch_size):
     train_loader = DataLoader(train_dataset, batch_size = batch_size, collate_fn=lambda data: collate(data, crop=crop))
     test_loader = DataLoader(test_dataset, batch_size = batch_size, collate_fn=lambda data: collate(data, crop=crop))
 
-    print(f"Num Train Batches: {len(train_loader)}, Test Batches: {len(test_loader)}")
-
     return train_loader, test_loader
     
 from torch.utils.data import Dataset, DataLoader
@@ -127,6 +125,8 @@ class HealthyData():
     def to(self, device):
        self.x.to(device)
        self.y.to(device)
+
+       return self
 
 
 class Healthy2Dataset(Dataset):
