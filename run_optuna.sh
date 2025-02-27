3b8781f19d2db510ae5d0a6892585a7c7c1f8aee
joobumjin <bumjin.joo@gmail.com>
2025-02-27 17:37:38 -0500

    misc preproc

--- run_optuna.sh
+++ run_optuna.sh
@@ -16,15 +16,16 @@ conda activate qbam
 # Run program
 cd /users/bjoo2/code/qbam/qbam_gnn
 
-declare -a arr=("TER" "VEGF" "Both")
+# declare -a arr=("TER" "VEGF" "Both")
+declare -a arr=("TER")
 for i in "${arr[@]}"
 do
     echo "Optuna Searching on $i"
     python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/   
 done
 
-for i in "${arr[@]}"
-do
-    echo "Optuna Searching Normalized $i"
-    python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/ --normed
-done
+# for i in "${arr[@]}"
+# do
+#     echo "Optuna Searching Normalized $i"
+#     python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/ --normed
+# done
