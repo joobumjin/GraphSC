f3e39f7d13f8ff2ae13a186b129f759041aa3e0e
joobumjin <bumjin.joo@gmail.com>
2025-03-20 23:39:19 -0400

    fix: test multi

--- run_optuna.sh
+++ run_optuna.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu --gres=gpu:1  --output=
+#SBATCH --partition=gpu --gres=gpu:1  --output=optuna_consec_data.out
 #SBATCH -N 1
 #SBATCH -n 4
 #SBATCH -t 60:00:00
