c64060df83686433a4e3d138bf18536793b01125
joobumjin <bumjin.joo@gmail.com>
2025-08-08 12:31:42 -0400

    fix: sbatch scripting

--- preprocessing/upsample.sh
+++ preprocessing/upsample.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu-debug --gres=gpu:1
+#SBATCH --partition=gpu --gres=gpu:1
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 00:48:00
-#SBATCH --mem=10g
+#SBATCH -n 4
+#SBATCH -t 60:00:00
+#SBATCH --mem=100g
 
 # Load a CUDA module
 module load cuda
 module load miniconda3/23.11.0s
-source /oscar/runtime/software/external/miniconda3/23.11.0/etc/profile.d/conda.sh
+source /gpfs/runtime/opt/miniconda/4.12.0/etc/profile.d/conda.sh
 
 conda activate cellpose3
 
