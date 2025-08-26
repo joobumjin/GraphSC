a127d28d40be8621679acd9d16d16cc29799e0bc
joobumjin <bumjin.joo@gmail.com>
2025-08-26 17:11:49 -0400

    fix: python imports

--- preprocessing/nyxus_test.py
+++ preprocessing/nyxus_test.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
-import tifffile
-import PIL
+from tifffile import imread
+from PIL import Image
 
 from nyxus import Nyxus
 nyx = Nyxus(["*ALL*"])
@@ -11,6 +11,6 @@ seg_dir = f"/users/bjoo2/scratch/LORD-2_upsample/npys"
 outline_dir = f"/users/bjoo2/scratch/LORD-2_upsample/outlines"
 mask_dir = f"/users/bjoo2/scratch/LORD-2_upsample/png_masks"
 
-image = tifffile.imread(f"{upsample_dir}/{os.listdir(upsample_dir)[0]}")
-mask = np.array(PIL.Image.open(f"{mask_dir}/{os.listdir(mask_dir)[0]}"))
+image = imread(f"{upsample_dir}/{os.listdir(upsample_dir)[0]}")
+mask = np.array(Image.open(f"{mask_dir}/{os.listdir(mask_dir)[0]}"))
 features = nyx.featurize(image, mask)
