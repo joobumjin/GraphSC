0211d7b5003c329fa44475b85c2e19a8df236038
joobumjin <bumjin.joo@gmail.com>
2025-09-22 19:48:33 -0400

    change: setup for optuna running 1-by-1

--- run_sh/run_optuna.sh
+++ run_sh/run_optuna.sh
@@ -17,12 +17,13 @@ conda activate qbam
 cd /users/bjoo2/code/qbam/qbam_gnn
 
 # declare -a arr=("TER" "VEGF" "Both")
+declare -a arr=("TER")
 # declare -a arr=("VEGF" "Both")
-# for i in "${arr[@]}"
-# do
-#     echo "Optuna Searching on $i"
-#     python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/
-#     # python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data/combined_data/graphs --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs
-# done
+for i in "${arr[@]}"
+do
+    echo "Optuna Searching on $i"
+    python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/
+    # python3 grid_search_optuna.py --data /users/bjoo2/data/bjoo2/qbam/data/combined_data/graphs --pred "$i" --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs
+done
 
-python3 grid_search_optuna.py --data /users/bjoo2/scratch/csam_data/csam15 --pred TER --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/ --study_name csam_data15
+# python3 grid_search_optuna.py --data /users/bjoo2/scratch/csam_data/csam15 --pred TER --log_path /users/bjoo2/code/qbam/qbam_gnn/optuna_logs/ --study_name csam_data15
