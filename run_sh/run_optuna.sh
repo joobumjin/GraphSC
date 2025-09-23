03490189392098e821a5103fac0895ee1d7ce79f
joobumjin <bumjin.joo@gmail.com>
2025-09-23 02:35:24 -0400

    run vegf

--- run_sh/run_optuna.sh
+++ run_sh/run_optuna.sh
@@ -17,7 +17,7 @@ conda activate qbam
 cd /users/bjoo2/code/qbam/qbam_gnn
 
 # declare -a arr=("TER" "VEGF" "Both")
-declare -a arr=("TER")
+declare -a arr=("VEGF")
 # declare -a arr=("VEGF" "Both")
 for i in "${arr[@]}"
 do
