1d68ef265a02b3e71a1a3a52f3ef3e701f4cdc56
joobumjin <bumjin.joo@gmail.com>
2025-09-27 18:52:34 -0400

    revert: just use old solution for now

--- preprocessing/img_preprocessing.py
+++ preprocessing/img_preprocessing.py
@@ -41,9 +41,8 @@ def get_image_loaders(data_dirs, target, batch_size):
     train_dfs = [pd.read_pickle(f"{train_pkl}") for train_pkl in train_pkls]
     val_df = pd.read_pickle(f"{valid_pkl}")
     test_df = pd.read_pickle(f"{test_pkl}")
-    print(test_df[0].y)
     
-    num_targets = len(test_df[0].y)
+    num_targets = 2 if target == "Both" else 1
 
     print(f"Constructing Dataloaders")
     train_loaders = [DataLoader(df, batch_size = batch_size, collate_fn=lambda data: collate(data, crop=crop)) for df in train_dfs]
