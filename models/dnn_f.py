c52688b3e5cfcbae6b660f309bda80a38dd054ff
joobumjin <bumjin.joo@gmail.com>
2025-10-01 16:36:36 -0400

    feat: reintroduce dep files

---
+++ models/dnn_f.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn.functional as F
+from torch.nn import Conv2d, MaxPool2d, AvgPool2d, BatchNorm2d, Sequential, Linear, Dropout
+
+class SameMaxPool2d(torch.nn.Module):
+    def __init__(self, kernel_size, stride=1):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def forward(self, data):
+        # Calculate padding
+        padding_h = (self.kernel_size - 1) // 2
+        padding_w = (self.kernel_size - 1) // 2
+
+        # Apply max pooling with calculated padding
+        return torch.nn.functional.max_pool2d(data, self.kernel_size, self.stride, padding=(padding_h, padding_w))
+
+class ConvBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super().__init__()
+        
+        self.kernel_size = kernel_size
+        self.conv = Conv2d(in_channels, out_channels, kernel_size, padding="same")
+        self.batchnorm2d = BatchNorm2d(out_channels)
+
+    def forward(self, data):
+        x = self.conv(data)
+        x = self.batchnorm2d(x)
+
+        return F.relu(x)
+
+class InceptionLayer(torch.nn.Module):
+    def __init__(self, channel_dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.oneConv = Conv2d(channel_dict["in"], channel_dict["oneConv"], (1,1))
+
+        self.threeConv_1 = Conv2d(channel_dict["in"], channel_dict["threeConv_1"], (1,1), padding="same")
+        self.threeConv_2 = Conv2d(channel_dict["threeConv_1"], channel_dict["threeConv_2"], (3,3), padding="same")
+
+        self.fiveConv_1 = Conv2d(channel_dict["in"], channel_dict["fiveConv_1"], (1,1), padding="same")
+        self.fiveConv_2 = Conv2d(channel_dict["fiveConv_1"], channel_dict["fiveConv_2"], (5,5), padding="same")
+
+        self.poolConv_1 = SameMaxPool2d(3)
+        self.poolConv_2 = Conv2d(channel_dict["in"], channel_dict["poolConv_2"], (5,5), padding="same")
+
+        self.batch_norm = BatchNorm2d(num_features=channel_dict["out"])
+
+    def forward(self, data):
+        oneConv_out = self.oneConv(data)
+
+        threeConv_out = self.threeConv_1(data)
+        threeConv_out = self.threeConv_2(threeConv_out)
+
+        fiveConv_out = self.fiveConv_1(data)
+        fiveConv_out = self.fiveConv_2(fiveConv_out)
+
+        poolConv_out = self.poolConv_1(data)
+        poolConv_out = self.poolConv_2(poolConv_out)
+
+        catted = torch.cat((oneConv_out, threeConv_out, fiveConv_out, poolConv_out), dim=1)        
+        return self.batch_norm(catted)
+
+class DNN_F(torch.nn.Module):
+    def __init__(self, output_dim):
+        super().__init__()
+
+        self.output_dim = output_dim
+
+        self.layers = Sequential(
+            Conv2d(3, 32, 3, padding="same"),
+            SameMaxPool2d(3, 2),
+            Conv2d(32, 32, 3, padding="same"),
+            SameMaxPool2d(3, 2),
+            Conv2d(32, 64, 3, padding="same"),
+            SameMaxPool2d(3, 2),
+            InceptionLayer(channel_dict={"in": 64, "oneConv": 32, "threeConv_1": 48, "threeConv_2": 64, "fiveConv_1": 8, "fiveConv_2": 16, "poolConv_2": 16, "out":128}),
+            SameMaxPool2d(3, 2),
+            InceptionLayer(channel_dict={"in": 128, "oneConv": 64, "threeConv_1": 96, "threeConv_2": 128, "fiveConv_1": 16, "fiveConv_2": 32, "poolConv_2": 32, "out":256}),
+            SameMaxPool2d(3, 2),
+            InceptionLayer(channel_dict={"in": 256, "oneConv": 192, "threeConv_1": 96, "threeConv_2": 208, "fiveConv_1": 16, "fiveConv_2": 48, "poolConv_2": 64, "out":512}),
+            InceptionLayer(channel_dict={"in": 512, "oneConv": 160, "threeConv_1": 112, "threeConv_2": 224, "fiveConv_1": 24, "fiveConv_2": 64, "poolConv_2": 64, "out":512}),
+            InceptionLayer(channel_dict={"in": 512, "oneConv": 160, "threeConv_1": 112, "threeConv_2": 224, "fiveConv_1": 24, "fiveConv_2": 64, "poolConv_2": 64, "out":512}),
+            SameMaxPool2d(3, 2),
+            InceptionLayer(channel_dict={"in": 512, "oneConv": 384, "threeConv_1": 192, "threeConv_2": 384, "fiveConv_1": 48, "fiveConv_2": 128, "poolConv_2": 128, "out":1024}),
+            InceptionLayer(channel_dict={"in": 1024, "oneConv": 384, "threeConv_1": 192, "threeConv_2": 384, "fiveConv_1": 48, "fiveConv_2": 128, "poolConv_2": 128, "out":1024}),
+            InceptionLayer(channel_dict={"in": 1024, "oneConv": 384, "threeConv_1": 192, "threeConv_2": 384, "fiveConv_1": 48, "fiveConv_2": 128, "poolConv_2": 128, "out":1024}),
+            SameMaxPool2d(3, 2),
+            InceptionLayer(channel_dict={"in": 1024, "oneConv": 384, "threeConv_1": 192, "threeConv_2": 384, "fiveConv_1": 48, "fiveConv_2": 128, "poolConv_2": 128, "out":1024}),
+            InceptionLayer(channel_dict={"in": 1024, "oneConv": 384, "threeConv_1": 192, "threeConv_2": 384, "fiveConv_1": 48, "fiveConv_2": 128, "poolConv_2": 128, "out":1024}),
+            InceptionLayer(channel_dict={"in": 1024, "oneConv": 384, "threeConv_1": 192, "threeConv_2": 384, "fiveConv_1": 48, "fiveConv_2": 128, "poolConv_2": 128, "out":1024}),
+            SameMaxPool2d(3, 2),
+            AvgPool2d((4,4)),
+            Conv2d(1024, output_dim, (1,1), padding="same")
+        )
+
+
+    def forward(self, data):
+        return F.relu(self.layers(data.x)).squeeze(dim=(2,3)) #squeeze width and height dimensions
---
+++ models/gnn_modular.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn.functional as F
+import torch_geometric.nn as geom
+from torch_geometric.nn import GraphConv, GCNConv, GATConv, GATv2Conv, global_mean_pool, BatchNorm
+import torch.nn as nn
+from torch.nn import Linear, Dropout, LeakyReLU
+import inspect
+
+def get_remaining_params(args, cls):
+    signature = inspect.signature(cls.__init__)
+    
+    parameters = [param.name for param in signature.parameters.values() if param.name != 'self']
+
+    return {key:args[key] for key in set(args.keys()).intersection(parameters)}
+
+class Modular_GNN(torch.nn.Module):
+    def __init__(self, num_node_features, output_dim,  num_dense = 2, num_gcn = 3, hidden_channels=128, dense_hidden=128, num_heads=8, dropout_p=0.5, gnn_layer = GATv2Conv):
+        super().__init__()
+        self.num_node_features = num_node_features
+        self.output_dim = output_dim
+        self.dense_hidden = dense_hidden
+        self.hidden_channels = hidden_channels
+        self.num_heads = num_heads
+        edge_dim = 0 if output_dim == 1 else 1
+
+        args = {"in_channels": self.num_node_features, 
+                "out_channels": self.hidden_channels, 
+                "heads": self.num_heads, 
+                "edge_dim": edge_dim, 
+                "concat": True,}
+        
+        args = get_remaining_params(args, gnn_layer)
+        if "heads" not in args.keys(): self.num_heads = 1
+
+        #build GAT Network
+        gat_layers = [
+            (gnn_layer(**args), 'x, edge_index -> x'),
+            (BatchNorm(self.hidden_channels * self.num_heads), 'x -> x'),
+            LeakyReLU(inplace=True)
+        ] #input layer
+
+        args["in_channels"] = self.hidden_channels * self.num_heads 
+        for _ in range(num_gcn - 2):
+            gat_layers += [
+              (gnn_layer(**args), 'x, edge_index -> x'),
+              (BatchNorm(self.hidden_channels * self.num_heads), 'x -> x'),
+              LeakyReLU(inplace=True)
+            ]
+      
+        args["out_channels"] = self.output_dim 
+        if "heads" in args.keys(): 
+          args["heads"] = 1
+          args["concat"] = False
+        gat_layers += [
+            (gnn_layer(**args), 'x, edge_index -> x'),
+            (BatchNorm(self.output_dim), 'x -> x')
+        ]
+
+        self.gat_net = geom.Sequential('x, edge_index', gat_layers)
+
+        #Build Dense Prediction Head
+        dense_layers = [
+          Linear(self.output_dim, self.dense_hidden),
+          LeakyReLU(inplace=True),
+          Dropout(p=dropout_p)
+        ]
+        for _ in range(num_dense - 2):
+          dense_layers += [
+            Linear(self.dense_hidden, self.dense_hidden),
+            LeakyReLU(inplace=True),
+            Dropout(p=dropout_p)
+          ]
+        dense_layers.append(Linear(self.dense_hidden, self.output_dim))
+
+        self.dense_head = nn.Sequential(*dense_layers)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        x = self.gat_net(x, edge_index)
+
+        x = global_mean_pool(x, batch)
+
+        x = self.dense_head(x)
+
+        return x
