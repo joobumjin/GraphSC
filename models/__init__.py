df976b2d5898b798f202cc14db7f46eed5f42b04
joobumjin <bumjin.joo@gmail.com>
2025-10-02 13:20:32 -0400

    fix: properly deprecate import'

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,4 @@ __all__ = ["dnn_f", "gnn_merge", "gnn_model", "gnn_modular"]
 # Import the submodules
 from models.dnn_f import *
 from models.gnn_merge import *
-from models.gnn_model import *
 from models.gnn_modular import *
