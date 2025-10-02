bf01d2083415f93f3b916acc730358f644a6e76e
joobumjin <bumjin.joo@gmail.com>
2025-10-02 14:38:55 -0400

    change some graph visualization params

--- utils/explanation_vis.py
+++ utils/explanation_vis.py
@@ -76,7 +76,7 @@ def _visualize_score(
 
     ax = df.plot(
         kind='barh',
-        figsize=(12, 10),
+        figsize=(14, 14),
         title=title,
         ylabel='Feature label',
         xlim=[0, float(df['score'].max()) + 0.3],
