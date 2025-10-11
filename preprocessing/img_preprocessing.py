6146391a086e37961368835133d6abc2010ea3a2
joobumjin <bumjin.joo@gmail.com>
2025-10-11 18:31:32 -0400

    test fix crop

--- preprocessing/img_preprocessing.py
+++ preprocessing/img_preprocessing.py
@@ -28,9 +28,9 @@ def get_image_loaders(data_dirs, target, batch_size, dataset="Healthy", crop=Tru
         #is it better to prealloc numpy?
         # images = torch.Tensor(np.transpose(np.array([sample.x for sample in data]), axes=(0,3,1,2)))
         images = np.zeros((batch_size, *image_shape)) #prealloc
-        for ind, sample in enumerate(data): images[ind] = sample.x #gather
+        for ind, sample in enumerate(data): images[ind] = crop_fn(sample.x) if crop_fn is not None else sample.x #gather
         images = torch.Tensor(images).permute(0,3,1,2) #transpose
-        if crop_fn is not None: images = crop_fn(images)
+        # if crop_fn is not None: images = crop_fn(images)
 
         labels = torch.Tensor(np.array([sample.y for sample in data]))[:, None]
 
